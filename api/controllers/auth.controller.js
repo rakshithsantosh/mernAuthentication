f80594d22694ebd970a3d6b04d493464da59030d
rakshith_s <rakshithsantosh@gmail.com>
2024-02-22 09:45:37 +0530

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
-import user from "../models/user.model";
+import user from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
-import { errorHandler } from "../utils/error";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req,res,next) =>{
     const {username,email,password}= req.body;
